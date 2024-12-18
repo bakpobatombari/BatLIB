17dba5c28f41a554315f469451be6889c55f68dd
bakpobatombari <122733659+bakpobatombari@users.noreply.github.com>
2024-12-18 18:51:01 +0100

    Add files via upload

---
+++ preloader.js
@@ -0,0 +1,6 @@
+window.addEventListener('DOMContentLoaded',() => {
+    $('.preloader').css({
+        "z-index": "-9999",
+        "display": "none"
+    })
+})
---
+++ script.js
@@ -0,0 +1,1 @@
+$('.slide').slideshow(5000);
